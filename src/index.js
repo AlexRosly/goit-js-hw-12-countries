cedb1748014ce2c1765db342d75ed2308c6886fa
AlexRosly <rosly.a777@gmail.com>
2021-07-27 14:14:57 +0300

    Made refactoring code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './css/styles.css';
-import './js/contry.js';
+// import './js/contry.js';
+import './js/country-refactoring.js';
---
+++ src/js/country-refactoring.js
@@ -0,0 +1,70 @@
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import countryCard from '../templates/country-card.hbs';
+import countryListTm from '../templates/country-list.hbs';
+import getRefs from './get-refs.js';
+import API from './api-service.js';
+
+
+const DEBOUNCE_DELAY = 300;
+
+const refs = getRefs();
+
+refs.inputValue.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY))
+
+function onSearch(e) {
+    e.preventDefault();
+    claerMarkup();
+
+    const name = e.target.value;
+    console.log(name.length);
+
+    if (name.trim('') === '') {
+        return;
+    } else if (name.length >= 1) {
+        API.fetchCountries(name)
+            .then(data => {
+                if (data.length === 1) {
+                    renderCountryCard(data, countryCard);
+                } else if (data.length >=2 && data.length <= 10) {
+                    renderCountryList(data, countryListTm);
+                } else {    
+                    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+                }
+       })
+       .catch(Error => {
+           Notiflix.Notify.failure('Oops, there is no country with that name.');
+       });
+    }
+       
+   
+};
+
+function claerMarkup() {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = '';
+};
+
+function renderCountryCard(country, template) {
+
+    const markupCountryCard = template(country[0]);
+    refs.countryInfo.insertAdjacentHTML('beforeend', markupCountryCard);
+    findCountryLang (country);
+
+}
+
+function renderCountryList(countryes, template) {
+       
+    const markupCountryList = countryes.map(country => template(country)).join('');
+    refs.countryList.insertAdjacentHTML('beforeend', markupCountryList);
+};
+
+
+function findCountryLang(data) {
+
+    const arrayLanguages = data.flatMap((languages) => languages.languages);
+    const makruplanguages = arrayLanguages.flatMap(data => data.name).join(', ');
+    const paragraph = document.querySelector('.country-languages');
+    paragraph.insertAdjacentHTML('beforeend', makruplanguages);
+    
+}
